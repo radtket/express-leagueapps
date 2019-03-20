49b5d7ae17f8be9dfa2243f3793e05b3fec5557c
Taylor Radtke <taylorradtke@gmail.com>
2019-03-19 22:28:32 -0400

    styed components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,45 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Table from "react-bootstrap/Table";
 import "./table.css";
+import { css, cx } from "emotion";
+const color = "white";
+
+const TeamColors = {
+	"2210410": {
+		primary: "#00BEFE",
+		secondary: "#000000",
+		text: "#FFFFFF"
+	},
+	"2210413": {
+		primary: "#FF9900",
+		secondary: "#7ABA05",
+		text: "#000000"
+	},
+	"2210416": {
+		primary: "#D12631",
+		secondary: "#FFFFFF",
+		text: "#1E1B1B"
+	},
+	"2210419": {
+		primary: "#8DBE23",
+		secondary: "#231F20",
+		text: "#FFFFFF"
+	},
+	"2210422": {
+		primary: "#9EC803",
+		secondary: "#35A701",
+		text: "#0C2103"
+	},
+	"2210425": {
+		primary: "#B81A2C",
+		secondary: "#FFBD00",
+		text: "#000000"
+	}
+};
 
 const App = () => {
 	const [schedule, setSchedule] = useState([]);
@@ -42,8 +74,6 @@ const App = () => {
 
 	const firstTeam = Object.values(schedule)[0];
 
-	console.log(schedule);
-
 	return (
 		<Container>
 			<div className="card">
@@ -53,15 +83,56 @@ const App = () => {
 							.sort()
 							.map((team, i) => {
 								const { name: teamName, id: teamId, games } = team[1];
+								const { primary, secondary, text } = TeamColors[teamId];
 								return (
 									<Tab eventKey={teamId} title={teamName} key={teamId}>
-										<header className="team-header">
+										<header
+											className={css`
+												background-color: ${primary};
+												background: linear-gradient(
+															to bottom,
+															${primary} 0%,
+															${primary} 38%,
+															transparent 100%
+														)
+														no-repeat,
+													${secondary};
+												position: relative;
+												display: flex;
+												align-items: center;
+												justify-content: center;
+												text-align: center;
+												padding: 3rem 0px;
+												color: ${text};
+												&::after {
+													content: "";
+													background: url("/img/logos/${teamId}.png") center
+														center/100% no-repeat;
+													opacity: 0.2;
+													top: 0;
+													left: 0;
+													bottom: 0;
+													right: 0;
+													position: absolute;
+													z-index: 0;
+												}
+												>* {
+													z-index: 2;
+												}
+												img {
+													height: 150px;
+												}
+											`}
+										>
 											<div className="container">
 												<img
 													src={`/img/logos/${teamId}.png`}
 													alt={`${teamName} Logo`}
 												/>
 												<h1>{teamName}</h1>
+												<h1 class="page-heading__title">
+													Team <span class="highlight">Schedule</span>
+												</h1>
 											</div>
 										</header>
 										<Table
@@ -82,8 +153,6 @@ const App = () => {
 													const {
 														gameId,
 														locationName,
-														locationId,
-														stateLabel,
 														team1,
 														team1Id,
 														team2,
